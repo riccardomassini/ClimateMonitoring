1117933fd7352d52fd29fa55408497b903d927d1
riccardomassini <massiniriccardo585@gmail.com>
2024-08-03 12:34:57 +0200

    change db name and sql commands

--- src/main/java/server/database/ImpostazioniDatabase.java
+++ src/main/java/server/database/ImpostazioniDatabase.java
@@ -34,7 +34,7 @@ class ImpostazioniDatabase {
     /**
      * Il nome del database al quale connettersi.
      */
-    static final String NOME_DATABASE = "climateMonitoring";
+    static final String NOME_DATABASE = "climatemonitoring";
 
     /**
      * L'URL di connessione al database, comprensivo della porta e del nome del database.
